070cd8e1cc0af95aa08e82ad90004530f9d83aad
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-08-19 16:30:56 +0900

    chore : babel 및 webpack 설정

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.jsx",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    historyApiFallback: true,
+    hot: true,
+  },
+  resolve: {
+    extensions: [".jsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+};
