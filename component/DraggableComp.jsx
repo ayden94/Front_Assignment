1a204f9100b9402a733964925d231f240a3fb95b
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-08-20 18:59:34 +0900

    feat : isDragging과 invalid 평가로 배경색 변경

--- component/DraggableComp.jsx
+++ component/DraggableComp.jsx
@@ -1,11 +1,20 @@
 import { Draggable } from "react-beautiful-dnd";
 import React from "react";
+import { useInvalid } from "../hooks/useInvalid";
 
 export default function DraggableComp({ item, index }) {
+  const [invalid] = useInvalid();
+  // console.log(invalid);
+
   return (
     <Draggable key={item.id} draggableId={item.id} index={index}>
-      {(provided) => (
-        <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+      {(provided, snapshot) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          className={`${snapshot.isDragging ? (invalid === index ? "bg-red-500" : "bg-green-400") : ""} p-2 rounded-md`}
+        >
           {item.content}
         </div>
       )}
