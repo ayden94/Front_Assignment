4fc7f11b7036a38678d5aee61bffd8f35995800c
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-08-20 15:59:53 +0900

    delete : 스타일 관련 함수 삭제

--- styles/getItemStyle.js
+++
@@ -1,9 +0,0 @@
-const GRID = 8;
-
-export const getItemStyle = (isDragging, draggableStyle) => ({
-  userSelect: "none",
-  padding: GRID * 2,
-  margin: `0 0 ${GRID}px 0`,
-  background: isDragging ? "lightgreen" : "grey",
-  ...draggableStyle,
-});
--- styles/getListStyle.js
+++
@@ -1,7 +0,0 @@
-const GRID = 8;
-
-export const getListStyle = (isDraggingOver) => ({
-  background: isDraggingOver ? "lightblue" : "lightgrey",
-  padding: GRID,
-  width: 250,
-});
