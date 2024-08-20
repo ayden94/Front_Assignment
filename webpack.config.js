cdbb6d006e4846f569b077a5180f4450d1e8591f
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-08-20 15:59:02 +0900

    chore : 웹팩 설정 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,35 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.jsx",
   output: {
-    filename: "main.js",
+    filename: "[name].[contenthash].js", // 각 청크에 대해 고유한 파일 이름 설정
     path: path.resolve(__dirname, "dist"),
+    clean: true, // 이전 빌드 파일을 정리
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./index.html",
+    }),
+    new CompressionPlugin({
+      filename: "[path][base].gz",
+      algorithm: "gzip",
+      test: /\.(js|css|html|svg)$/,
+      threshold: 10240,
+      minRatio: 0.8,
     }),
   ],
   devServer: {
     static: path.resolve(__dirname, "dist"),
     historyApiFallback: true,
     hot: true,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
   },
   resolve: {
     extensions: [".jsx", ".js"],
