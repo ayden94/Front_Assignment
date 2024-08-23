fa115e40867a3834f8a95a12fb0a3a4916867dde
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-08-23 18:38:45 +0900

    refactor : 배경색 결정 함수 분리

--- component/DraggableComp.jsx
+++ component/DraggableComp.jsx
@@ -1,18 +1,18 @@
 import { Draggable } from "react-beautiful-dnd";
 import React from "react";
 import { useInvalid } from "../hooks/globalState/useInvalid";
+import { useSelectedList } from "../hooks/globalState/useSelectedList";
+import { useIsDragging } from "../hooks/globalState/useIsDragging";
+import { isMultiDrag } from "../util/isMultiDrag";
 
-export default function DraggableComp({ item, index }) {
-  const [invalid] = useInvalid();
-
+export default function DraggableComp({ item, index, onClick }) {
   return (
     <Draggable key={item.id} draggableId={item.id} index={index}>
       {(provided, snapshot) => (
         <div
+          onClick={onClick}
           ref={provided.innerRef}
-          className={`${
-            snapshot.isDragging ? (invalid === item.id ? "bg-red-500" : "bg-green-400") : "hover:bg-gray-300"
-          } p-2 rounded-md`}
+          className={`${getBackgroundColor(snapshot, item.id)} p-2 rounded-md`}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
         >
@@ -22,3 +22,21 @@ export default function DraggableComp({ item, index }) {
     </Draggable>
   );
 }
+
+const getBackgroundColor = (snapshot, itemId) => {
+  const [invalid] = useInvalid();
+  const [selectedList] = useSelectedList();
+  const [isDragging] = useIsDragging();
+
+  if (!isMultiDrag(selectedList) && snapshot.isDragging) {
+    if (invalid === itemId) return "bg-red-500";
+    else return "bg-green-400";
+  }
+
+  if (selectedList.includes(itemId)) {
+    if (isDragging) return "bg-blue-200";
+    else return "bg-blue-400";
+  }
+
+  return "hover:bg-gray-300";
+};
